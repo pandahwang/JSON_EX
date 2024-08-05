ea5d3c263be504947ce4de3bd00f1d0ab42682ec
pandahwang <pandahwang@naver.com>
2024-08-05 16:18:02 +0900

    init: add files

---
+++ src/main/java/com/panda/JSON_EX/JSON_EX/JsonExApplication.java
@@ -0,0 +1,13 @@
+package com.panda.JSON_EX.JSON_EX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panda/JSON_EX/JSON_EX/JsonExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panda.JSON_EX.JSON_EX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
