b3c4318af8607550459cd588a640be11a084da0b
pandahwang <pandahwang@naver.com>
2024-08-08 17:03:19 +0900

    mod: add checkKey() method, checks if json data key is changed

--- src/main/java/com/panda/JSON_EX/JSON_EX/User/UserService.java
+++ src/main/java/com/panda/JSON_EX/JSON_EX/User/UserService.java
@@ -1,5 +1,4 @@
 package com.panda.JSON_EX.JSON_EX.User;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -19,20 +19,53 @@ public class UserService {
 
     // json 파일을 읽어와서 DB에 저장하는 메소드
     public void update() {
-        try{
+        try {
 
-        // ClassPathResource로 user-data.json 파일을 로드
-        ClassPathResource resource = new ClassPathResource("user-data.json");
+            // ClassPathResource로 user-data.json 파일을 로드
+            ClassPathResource resource = new ClassPathResource("user-data.json");
 
-        // JSON 파일을 읽어 User 객체 리스트로 변환
-        List<User> users = objectMapper.readValue(resource.getInputStream(), new TypeReference<List<User>>() {});
+            // JSON 파일을 읽어 User 객체 리스트로 변환
+            List<User> users = objectMapper.readValue(resource.getInputStream(), new TypeReference<List<User>>() {
+            });
 
-        // 변환한 User 객체 리스트를 DB에 저장
-        userRepository.saveAll(users);
+            // 변환한 User 객체 리스트를 DB에 저장
+            userRepository.saveAll(users);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    // json 데이터 key 값을 확인 하는 메서드
+
+    public boolean checkKey() {
+        boolean isKeyChanged = false;
+        try {
+            String[] keys = {"id", "username", "post_count"};
+
+            // ClassPathResource로 user-data.json 파일을 로드
+            ClassPathResource resource = new ClassPathResource("user-data.json");
+
+            // JSON 파일을 읽어 Map 객체 리스트로 변환
+            List<Map<String, Object>> data = objectMapper.readValue(resource.getInputStream(), new TypeReference<List<Map<String, Object>>>() {
+            });
+
+            // json 파일 key 값 확인
+            System.out.println(data.get(0).keySet());
+            if (data.get(0).keySet().equals(keys)) {
+                System.out.println("key 값 일치");
+                isKeyChanged = true;
+            } else {
+                System.out.println("key 값 불일치");
+                isKeyChanged = false;
+            }
+
+            // 변환한 User 객체 리스트를 DB에 저장
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isKeyChanged;
+    }
+
 
     // 모든 사용자의 post_count를 출력하는 메소드
     public int sum() {
