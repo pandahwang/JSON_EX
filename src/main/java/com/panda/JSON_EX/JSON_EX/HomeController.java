c8acfd84c5a9996ec7ccad4bf8de2ded4609b8da
pandahwang <pandahwang@naver.com>
2024-08-18 21:51:15 +0900

    HomeController 추가

---
+++ src/main/java/com/panda/JSON_EX/JSON_EX/HomeController.java
@@ -0,0 +1,13 @@
+package com.panda.JSON_EX.JSON_EX;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Authentication auth) {
+        return "index.html";
+    }
+}
